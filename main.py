98a2950d3a88ee6a504ecfeaf02c5a7aaa3b770c
김세현 <dnjfdid14@naver.com>
2022-12-02 16:03:20 +0900

    ResNet

--- main.py
+++ main.py
@@ -2,3 +2,17 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+
+class BasicBlock(nn.Module):]
+    mul = 1
+
+    def __init__(self, in_planes, out_planes, stride=1):
+        super(BasicBlock, self).__init__()
+
+        self.conv1 = nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(out_planes)
+
+        self.conv2 = nn.Conv2d(out_planes, out_planes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(out_planes)
+
+        
