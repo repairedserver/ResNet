c01f07d751b6c608935b9ba28b4561240873b7d9
김세현 <dnjfdid14@naver.com>
2022-12-02 16:09:02 +0900

    ResNet

--- main.py
+++ main.py
@@ -69,3 +69,43 @@ class BottleNeck(nn.Module):
         out += self.shortcut(x)
         out = F.relu(out)
         return out
+
+
+class ResNet(nn.Module):
+    def __init__(self, block, num_blocks, num_classes=10):
+        super(ResNet, self).__init__()
+        self.in_planes = 64
+
+        self.conv1 = nn.Conv2d(3, self.in_planes, kernel_size=7, stride=2, padding=3)
+        self.bn1 = nn.BatchNorm2d(self.in_planes)
+        self.maxpool1 = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+        self.layer1 = self.make_layer(block, 64, num_blocks[0], stride=1)
+        self.layer2 = self.make_layer(block, 128, num_blocks[1], stride=2)
+        self.layer3 = self.make_layer(block, 256, num_blocks[2], stride=2)
+        self.layer4 = self.make_layer(block, 512, num_blocks[3], stride=2)
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+
+        self.linear = nn.Linear(512 * block.mul, num_classes)
+
+    def make_layer(self, block, out_planes, num_blocks, stride):
+        strides = [stride] + [1] * (num_blocks - 1)
+        layers = []
+        for i in range(num_blocks):
+            layers.append(block(self.in_planes, out_planes, strides[i]))
+            self.in_planes = block.mul * out_planes
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = F.relu(out)
+        out = self.maxpool1(out)
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = self.avgpool(out)
+        out = torch.flatten(out, 1)
+        out = self.linear(out)
+        return out
