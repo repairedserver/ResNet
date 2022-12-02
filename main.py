75e1c792c46aa098f74e354a4d24419abd9dba52
김세현 <dnjfdid14@naver.com>
2022-12-02 16:05:03 +0900

    ResNet

--- main.py
+++ main.py
@@ -32,4 +32,28 @@ class BasicBlock(nn.Module):
         out += self.shortcut(x)
         out = F.relu(out)
         return out
-    
+
+
+class BottleNeck(nn.Module):
+    mul = 4
+
+    def __init__(self, in_planes, out_planes, stride=1):
+        super(BottleNeck, self).__init__()
+
+        self.conv1 = nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
+        self.bn1 = nn.BatchNorm2d(out_planes)
+
+        self.conv2 = nn.Conv2d(out_planes, out_planes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(out_planes)
+
+        self.conv3 = nn.Conv2d(out_planes, out_planes * self.mul, kernel_size=1, stride=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(out_planes * self.mul)
+
+        self.shortcut = nn.Sequential()
+
+        if stride != 1 or in_planes != out_planes * self.mul:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, out_planes * self.mul, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(out_planes * self.mul)
+            )
+            
